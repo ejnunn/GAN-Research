6039f062dc9ac3fb2412c8fa5cb28f24e4402f97
Eric Nunn <ejnunn1@msn.com>
2021-02-26 12:11:15 -0800

    Create datasets.py
    ImageDataset class definition for GAN datasets

---
+++ datasets.py
@@ -0,0 +1,40 @@
+import glob
+import random
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+# Inspired by https://github.com/aitorzip/PyTorch-CycleGAN/blob/master/datasets.py
+class ImageDataset(Dataset):
+    '''
+    Parameters:
+    max_size - limit dataset size to ensure effective epoch training duration
+    '''
+    def __init__(self, root, transform=None, mode='train', max_size=10000):
+        self.transform = transform
+        self.files_A = sorted(glob.glob(os.path.join(root, '%sA' % mode) + '/*.*'))[:max_size]
+        self.files_B = sorted(glob.glob(os.path.join(root, '%sB' % mode) + '/*.*'))[:max_size]
+        if len(self.files_A) > len(self.files_B):
+            self.files_A, self.files_B = self.files_B, self.files_A
+        self.new_perm()
+        assert len(self.files_A) > 0, "Make sure you downloaded the correct images!"
+
+    def new_perm(self):
+        self.randperm = torch.randperm(len(self.files_B))[:len(self.files_A)]
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        item_B = self.transform(Image.open(self.files_B[self.randperm[index]]))
+
+        # Convert B&W images into RGB color
+        if item_A.shape[0] != 3: 
+            item_A = item_A.repeat(3, 1, 1)
+        if item_B.shape[0] != 3: 
+            item_B = item_B.repeat(3, 1, 1)
+        if index == len(self) - 1:
+            self.new_perm()
+        # Old versions of PyTorch didn't support normalization for different-channeled images
+        return (item_A - 0.5) * 2, (item_B - 0.5) * 2
+
+    def __len__(self):
+        return min(len(self.files_A), len(self.files_B))
