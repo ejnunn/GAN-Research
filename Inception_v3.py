182de6e041b78d1c1210aa43d92a51a693b6d087
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-03-06 19:59:05 -0800

    added flag to Inception model for FID and new FID feature outputs

--- Inception_v3.py
+++ Inception_v3.py
@@ -12,7 +12,7 @@ import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, Batch
 
 
 class GoogLeNet(nn.Module):
-    def __init__(self, aux_logits=True, num_classes=1000):
+    def __init__(self, aux_logits=True, num_classes=1000, include_top=True, all_feature_maps=False):
         super(GoogLeNet, self).__init__()
         assert aux_logits == True or aux_logits == False
         self.aux_logits = aux_logits
@@ -34,19 +34,19 @@ class GoogLeNet(nn.Module):
         # In this order: in_channels, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, out_1x1pool
         self.inception3a = Inception_block(192, 64, 96, 128, 16, 32, 32)
         self.inception3b = Inception_block(256, 128, 128, 192, 32, 96, 64)
-        self.maxpool3 = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)	# New Feature Maps
 
         self.inception4a = Inception_block(480, 192, 96, 208, 16, 48, 64)
         self.inception4b = Inception_block(512, 160, 112, 224, 24, 64, 64)
         self.inception4c = Inception_block(512, 128, 128, 256, 24, 64, 64)
         self.inception4d = Inception_block(512, 112, 144, 288, 32, 64, 64)
         self.inception4e = Inception_block(528, 256, 160, 320, 32, 128, 128)
-        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)		# New Feature Maps
 
         self.inception5a = Inception_block(832, 256, 160, 320, 32, 128, 128)
         self.inception5b = Inception_block(832, 384, 192, 384, 48, 128, 128)
 
-        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1)
+        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1) 					# FID Feature Maps
         self.dropout = nn.Dropout(p=0.4)
         self.fc1 = nn.Linear(1024, 1000)
 
@@ -65,7 +65,8 @@ class GoogLeNet(nn.Module):
 
         x = self.inception3a(x)
         x = self.inception3b(x)
-        x = self.maxpool3(x)
+        x = self.maxpool3(x)		# New Feature Maps
+        feature_map_1 = x
 
         x = self.inception4a(x)
 
@@ -82,16 +83,24 @@ class GoogLeNet(nn.Module):
             aux2 = self.aux2(x)
 
         x = self.inception4e(x)
-        x = self.maxpool4(x)
+        x = self.maxpool4(x)		# New Feature Maps
+        feature_map_2 = x
         x = self.inception5a(x)
         x = self.inception5b(x)
-        x = self.avgpool(x)
+        x = self.avgpool(x)			# FID Feature Maps (i.e. include_top=False)
+        feature_map_3 = x
         x = x.reshape(x.shape[0], -1)
         x = self.dropout(x)
         x = self.fc1(x)
 
         if self.aux_logits and self.training:
             return aux1, aux2, x
+        elif self.all_feature_maps:
+        	# New FID
+        	return [feature_map_1, feature_map_2, feature_map_3]
+       	elif not self.include_top:
+       		# Original FID
+       		return feature_map_3
         else:
             return x
 
